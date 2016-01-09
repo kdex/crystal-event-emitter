0f369bfe3af98f6b1450358c4dd1526d5a5d67a3
kdex <kdex@kdex.de>
2016-01-09 02:11:55 +0100

    Fixed a bug where emitting a number broke inference

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -50,7 +50,7 @@ export class EventEmitter {
 	emit(event, ...args) {
 		let inferenceSuccessful = false;
 		/* Treat inferred listeners first */
-		if (this.options.inferListeners) {
+		if (this.options.inferListeners && typeof event === "string") {
 			let inferredListener = `on${event[0].toUpperCase()}${event.substr(1)}`;
 			if (this[inferredListener] instanceof Function) {
 				inferenceSuccessful = true;
