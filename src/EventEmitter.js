5b9fac0987bdd3d2397209c47ba60d26babfa51f
kdex <kdex@kdex.de>
2016-04-07 04:54:56 +0200

    Stopped inheriting internal variables

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -1,19 +1,22 @@
+const EXTENSIONS = Symbol("[[Extensions]]");
 export class EventEmitter {
 	constructor(options = {}) {
 		if (!(options instanceof Object)) {
 			throw new TypeError();
 		}
-		this.options = options;
-		this.events = new Map();
+		this[EXTENSIONS] = {
+			options,
+			events: new Map()
+		};
 	}
 	addEventListener(event, callback) {
 		if (callback instanceof Function) {
 			/* Retrieve the listeners for this event type */
-			let callbacks = this.events.get(event);
+			let callbacks = this[EXTENSIONS].events.get(event);
 			/* Add a new set if necessary */
 			if (!callbacks || !callbacks.size) {
 				callbacks = new Set();
-				this.events.set(event, callbacks);
+				this[EXTENSIONS].events.set(event, callbacks);
 			}
 			/* Add a callback if necessary */
 			if (!callbacks.has(callback)) {
@@ -28,7 +31,7 @@ export class EventEmitter {
 	removeEventListener(event, callback) {
 		if (callback instanceof Function) {
 			/* Retrieve the listeners for this event type */
-			let callbacks = this.events.get(event);
+			let callbacks = this[EXTENSIONS].events.get(event);
 			/* Add a new set if necessary */
 			if (!callbacks) {
 				return this;
@@ -39,7 +42,7 @@ export class EventEmitter {
 			}
 			/* Remove the event if necessary */
 			if (!callbacks.size) {
-				this.events.delete(event);
+				this[EXTENSIONS].events.delete(event);
 			}
 			return this;
 		}
@@ -50,14 +53,14 @@ export class EventEmitter {
 	emit(event, ...args) {
 		let inferenceSuccessful = false;
 		/* Treat inferred listeners first */
-		if (this.options.inferListeners && typeof event === "string") {
+		if (this[EXTENSIONS].options.inferListeners && typeof event === "string") {
 			let inferredListener = `on${event[0].toUpperCase()}${event.substr(1)}`;
 			if (this[inferredListener] instanceof Function) {
 				inferenceSuccessful = true;
 				this[inferredListener].apply(this, args);
 			}
 		}
-		let callbacks = this.events.get(event);
+		let callbacks = this[EXTENSIONS].events.get(event);
 		if (!callbacks || !callbacks.size) {
 			return inferenceSuccessful;
 		}
