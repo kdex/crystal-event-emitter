541d8615948b8f7cd9265596ec9ff7f37cc8b1ae
kdex <kdex@kdex.de>
2017-07-31 00:06:13 +0200

    Updated dependencies, removed deprecated option

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,6 @@
 import gulp from "gulp";
 import babel from "gulp-babel";
 import esLint from "gulp-eslint";
-import ava from "gulp-ava";
-export function test() {
-	return gulp.src("test/**/*.js")
-		.pipe(ava({
-			verbose: true
-		}));
-}
 export function js() {
 	return gulp.src("src/**/*.js")
 		.pipe(babel())
