8c5704ae13fa12f9a73a6813a4c2084952564311
kdex <kdex@kdex.de>
2018-02-02 00:09:35 +0100

    Fixed wrong signature

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -47,7 +47,7 @@ export class EventEmitter {
 		* @type {object}
 		* @property {object} this[EXTENSIONS].options
 		* See {@link constructor}.
-		* @property {Map<string|symbol, function[]>} this[EXTENSIONS].events
+		* @property {Map<string|symbol, Set<function>>} this[EXTENSIONS].events
 		* The internal storage for event listeners
 		*/
 		this[EXTENSIONS] = {
